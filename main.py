8dc2a3a1255b62fe096fc7c08b678f61f4d6c848
juyoon <juyoon0423@daum.net>
2025-09-09 18:34:22 +0900

    POST 요청 추가

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Pydantic 모델 정의
+class Item(BaseModel):
+    name: str
+    price: float
+
+# GET 요청
 @app.get("/")
 def read_root():
     return {"message": "Hello FastAPI!"}
+
+# POST 요청
+@app.post("/items/")
+def create_item(item: Item):
+    return {"item_name": item.name, "item_price": item.price}
